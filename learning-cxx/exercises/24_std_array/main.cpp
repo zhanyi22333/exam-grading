79d5ce38aa4c5705220eb9b1f10abcddf511aa6d
Zhang Yi <18994118902@163.com>
2025-01-17 18:15:32 +0800

    Add cpp answers version 1.

---
+++ learning-cxx/exercises/24_std_array/main.cpp
@@ -0,0 +1,28 @@
+﻿#include "../exercise.h"
+#include <array>
+#include <cstring>
+
+// READ: std::array <https://zh.cppreference.com/w/cpp/container/array>
+
+// TODO: 将下列 `?` 替换为正确的代码
+int main(int argc, char **argv) {
+    {
+        std::array<int, 5> arr{{1, 2, 3, 4, 5}};
+        ASSERT(arr.size() == 5, "Fill in the correct value.");
+        ASSERT(sizeof(arr) == 5*4, "Fill in the correct value.");
+        int ans[]{1, 2, 3, 4, 5};
+        ASSERT(std::memcmp(arr.data(), ans, 5) == 0, "Fill in the correct values.");
+    }
+    {
+        std::array<double, 8> arr;
+        ASSERT(arr.size() == 8, "Fill in the correct value.");
+        ASSERT(sizeof(arr) == 8*8, "Fill in the correct value.");
+    }
+    {
+        std::array<char, 21> arr{"Hello, InfiniTensor!"};
+        ASSERT(arr.size() == 21, "Fill in the correct value.");
+        ASSERT(sizeof(arr) == 21*1, "Fill in the correct value.");
+        ASSERT(std::strcmp(arr.data(), "Hello, InfiniTensor!") == 0, "Fill in the correct value.");
+    }
+    return 0;
+}
